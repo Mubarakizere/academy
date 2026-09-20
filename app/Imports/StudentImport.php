bc352d36057a4f2b50075b5f7b76e2293c325b9d
Izere Moubarak <izeremubarak05@gmail.com>
2026-09-20 18:38:35 +0200

    fix: upgrade maatwebsite/excel to v4 for PHP 8.5 compatibility

--- app/Imports/StudentImport.php
+++ app/Imports/StudentImport.php
@@ -5,6 +5,7 @@ namespace App\Imports;
 use App\Models\Classroom;
 use App\Models\Student;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
@@ -18,7 +19,7 @@ class StudentImport implements ToModel, WithHeadingRow, WithValidation, SkipsOnF
 
   public int $importedCount = 0;
 
-  public function model(array $row)
+  public function model(array $row): ?Model
   {
     $classroom = Classroom::where('name', $row['kelas'])->first();
 
